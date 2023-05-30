70f6a9cf86b1d383020b2f375673e11d0ff9d2cc
XHKIBRAHIM <xhkibrahim732@gmail.com>
2023-05-30 09:14:00 +0500

    themes

---
+++ Practice 7/script.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById("btn");
+var pera = document.getElementById("pera");
+var bd = document.getElementById("bd");
+
+function btnPera() {
+    btn.classList.toggle("act")
+    pera.classList.toggle("on")
+    bd.classList.toggle("dark")
+}
